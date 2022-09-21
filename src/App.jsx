fe125d2d9866f6f1b254c824279e4e4201386cd0
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-09-21 19:58:50 +0200

    Add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <h1>Entretien React</h1>
+      <h2>Compteur</h2>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>Incrémeter</button>
+    </>
+  );
 };
 
 export default App;
