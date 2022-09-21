d00ed5eee6edfc4f546677195359f718099d1741
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-09-21 21:58:43 +0200

    Add API interview test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const url = 'https://randomuser.me/api?page=';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [user, setUser] = useState('');
+  const [people, setPeople] = useState([]);
+  const [pageIndex, setPageIndex] = useState(1);
+
+  const fetchRandomUser = async () => {
+    try {
+      const {
+        data: { results }
+      } = await axios(`${url}${pageIndex}`);
+      const user = results[0];
+      setUser(user);
+      setPeople([...people, user]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomUser();
+  }, []);
 
   return (
     <>
@@ -9,6 +32,23 @@ const App = () => {
       <h2>Compteur</h2>
       <p>{counter}</p>
       <button onClick={() => setCounter(counter + 1)}>Incrémeter</button>
+      <h2>API</h2>
+      {/* <pre>{user && JSON.stringify(user, null, 2)}</pre> */}
+      {people.map((person, index) => {
+        const {
+          name: { first: name },
+          picture: { thumbnail: img }
+        } = person;
+        return (
+          <div key={index}>
+            <img src={img} alt={name} />
+            <p>{name}</p>
+          </div>
+        );
+      })}
+      <button onClick={() => setPageIndex(pageIndex + 1)}>
+        charger plus d'utilisateur
+      </button>
     </>
   );
 };
