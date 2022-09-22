8126f5f336e8d8ba8525dd78c65d61288c49790b
Fixito <thomas_3004@hotmail.fr>
2022-09-22 10:44:39 +0200

    Add CRA linter and fix warnings

---
+++ README.md
@@ -0,0 +1,23 @@
+# Entretien REACT
+
+## Installer le linter de create-react-app
+
+```
+npm i -D eslint-config-react-app
+```
+
+Créer un fichier .eslintrc.json à la racine du projet avec le contenu suivant :
+
+```JSON
+{
+  "extends": "react-app"
+}
+```
+
+## Niveau Débutant
+
+### Compteur
+
+Créer un compteur commençant à 0 ainsi qu'un bouton permettant de l'incrémenter de 1.
+
+### API
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 const url = 'https://randomuser.me/api?page=';
@@ -9,36 +9,37 @@ const App = () => {
   const [people, setPeople] = useState([]);
   const [pageIndex, setPageIndex] = useState(1);
 
-  const fetchRandomUser = async () => {
+  const fetchRandomUser = useCallback(async () => {
     try {
       const {
         data: { results }
       } = await axios(`${url}${pageIndex}`);
       const user = results[0];
       setUser(user);
-      setPeople([...people, user]);
+      setPeople((people) => [...people, user]);
     } catch (error) {
       console.log(error);
     }
-  };
+  }, [pageIndex]);
 
   useEffect(() => {
     fetchRandomUser();
-  }, []);
+  }, [pageIndex, fetchRandomUser]);
 
   return (
     <>
       <h1>Entretien React</h1>
       <h2>Compteur</h2>
       <p>{counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>Incrémeter</button>
+      <button onClick={() => setCounter(counter + 1)}>Incrémenter</button>
       <h2>API</h2>
-      {/* <pre>{user && JSON.stringify(user, null, 2)}</pre> */}
+      <pre>{user && JSON.stringify(user, null, 2)}</pre>
       {people.map((person, index) => {
         const {
           name: { first: name },
           picture: { thumbnail: img }
         } = person;
+
         return (
           <div key={index}>
             <img src={img} alt={name} />
@@ -47,7 +48,7 @@ const App = () => {
         );
       })}
       <button onClick={() => setPageIndex(pageIndex + 1)}>
-        charger plus d'utilisateur
+        Charger plus d'utilisateurs
       </button>
     </>
   );
